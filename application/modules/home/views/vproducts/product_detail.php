6ef6a79ca1345adf54c224601158c8e7a74fad5b
thanhdo.trinh@gmail.com <thanhdo.trinh@gmail.com@1fa18133-fb69-3655-42be-f4629fbfda3d>
2014-03-19 10:24:48 +0000

    thieu thu vien chi tiet sp

--- application/modules/home/views/vproducts/product_detail.php
+++ application/modules/home/views/vproducts/product_detail.php
@@ -1,51 +1,93 @@
 <script src="<?php echo base_url(); ?>template/js/jquery.easytabs.min.js" type="text/javascript"></script>
-<?php foreach ($data_detail as $value)?>
-<section class="bg_shadow">
-    <div class="wrap clearfix">
-        <div class="title floatLeft">
-            <h6>Chi tiết sản phẩm</h6>
-            <span>Thông tin chi tiết sản phẩm và nhà cung cấp</span>
-        </div>
-        <div class="clearfix breadcrumbs floatRight">
-            <div class="fl" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
-                <a title="Trang nhất" href="/" itemprop="url">
-                    <span itemprop="title">Home</span>
-                </a> /
-            </div>
-            <div itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb" class="fl">
-                <a class="highlight" href="/" title="Kiến thức SEO" itemprop="url">
-                    <span itemprop="title">Product</span>
-                </a>
-            </div>
-        </div>
-    </div>
-</section>
-<section id="content" class="wrap">
+<script type="text/javascript" src="<?php echo base_url(); ?>public/homejs/jquery.jqzoom-core.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>public/homejs/jquery.cslider.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>public/homejs/modernizr.custom.17475.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>public/homejs/jquery.elastislide.js"></script>
+<script type="text/javascript">
 
-    <section id="primary">
-        <section class="product_detail">
-            <div id="product_view">
-                <a href="<?php echo base_url(); ?>template/uploads/29672_250008_64788_zoom.jpg" class="jqzoom" rel='gal1'  title="triumph">
+    jQuery(document).ready(function() {
+        //
+        jQuery('#da-slider').cslider({
+            autoplay: true,
+            bgincrement: 450
+        });
+        //
+        $(".form").h5Validate({
+            errorClass: "validationError",
+            validClass: "validationValid"
+        });
+
+        // Prevent form submission when errors
+        $(".form").submit(function(evt) {
+            if ($(".form").h5Validate("allValid") === false) {
+                evt.preventDefault();
+            }
+        });
+        jQuery('#carousel').elastislide();
+
+        jQuery('.jqzoom').jqzoom({
+            zoomType: 'standard',
+            lens: true,
+            preloadImages: false,
+            alwaysOn: false
+        });
+    });
+</script>
+<?php foreach ($data_detail as $value)
+     ?>
+    <section class="bg_shadow">
+        <div class="wrap clearfix">
+            <div class="title floatLeft">
+                <h6>Chi tiết sản phẩm</h6>
+                <span>Thông tin chi tiết sản phẩm và nhà cung cấp</span>
+            </div>
+            <div class="clearfix breadcrumbs floatRight">
+                <div class="fl" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+                    <a title="Trang nhất" href="/" itemprop="url">
+                        <span itemprop="title">Home</span>
+                    </a> /
+                </div>
+                <div itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb" class="fl">
+                    <a class="highlight" href="/" title="Kiến thức SEO" itemprop="url">
+                        <span itemprop="title">Product</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="content" class="wrap">
+
+        <section id="primary">
+            <section class="product_detail">
+                <div id="product_view">
+                    <a href="<?php echo base_url(); ?>template/uploads/29672_250008_64788.jpg" class="jqzoom" rel='gal1'  title="triumph">
                     <img class="view" src="<?php echo base_url(); ?>template/uploads/29672_250008_64788.jpg" alt=""  title="triumph"/>
                 </a>
-                <div class="clear">                
-                </div>
+                <div class="clear"></div>
                 <div id="sliderthumb">
                     <ul  id="carousel" class="elastislide-list">
-                        <li><a  class="zoomThumbActive" href='javascript:void(0);' rel="{gallery: 'gal1', smallimage: './uploads/29672_250008_64788.jpg',largeimage: './uploads/29672_250008_64788_zoom.jpg'}"><img src="uploads/29672_250008_64788.jpg"/></a></li>
-                        <li><a  href='javascript:void(0);' rel="{gallery: 'gal1', smallimage: './uploads/30087_255978_64112.jpg',largeimage: './uploads/30087_255978_64112.jpg'}"><img src="uploads/30087_255978_64112.jpg"/></a></li>
-                        <li><a  href='javascript:void(0);' rel="{gallery: 'gal1', smallimage: './uploads/29717_250549_64037.jpg',largeimage: './uploads/29717_250549_64037.jpg'}"><img src='uploads/29717_250549_64037.jpg'></a></li>
+                        <li>
+                            <a  href='javascript:void(0);' rel="{gallery: 'gal1', smallimage: './uploads/30087_255978_64112.jpg',largeimage: './uploads/30087_255978_64112.jpg'}">
+                                <img src="uploads/30087_255978_64112.jpg"/></a>
+                        </li>
+                        <li>
+                            <a  href='javascript:void(0);' rel="{gallery: 'gal1', smallimage: './uploads/29717_250549_64037.jpg',largeimage: './uploads/29717_250549_64037.jpg'}">
+                                <img src='uploads/29717_250549_64037.jpg'></a>
+                        </li>
+                        <li>
+                            <a href='javascript:void(0);' rel="{gallery: 'gal1', smallimage: './uploads/29717_250549_64038.jpg',largeimage: './uploads/29717_250549_64039.jpg'}">
+                                <img src='uploads/29717_250549_64038.jpg'></a>
+                        </li>
                     </ul>
                 </div>
             </div>
-
             <section id="product_content">
                 <h1><?php echo $value->name ?></h1>
                 <header class="longer_products">
                     <h6 class="icon_longer_products">Còn hàng</h6>
                 </header>
                 <div class="subdetail">
-                    <?php echo $value->intro ?>
+<?php echo $value->intro ?>
                 </div>
 
                 <div class="price_3">
@@ -82,7 +124,7 @@
                                 <h6 class="title_detail_item">Đặc điểm nổi bật</h6>
                                 <ul>
                                     <li>
-                                        <?php echo $value->hightlight ?> 
+<?php echo $value->hightlight ?> 
                                     </li>
                                 </ul>
                             </div>
@@ -91,7 +133,7 @@
                                 <ul>
                                     <li>
 
-                                       <?php echo $value->condition ?>
+<?php echo $value->condition ?>
 
                                     </li>
                                 </ul>
@@ -100,7 +142,7 @@
                             <div class="detail_item_post">
                                 <header class="title_post_detail">Detail information</header>
                                 <p>
-                                    <?php echo $value->productinfo ?>
+<?php echo $value->productinfo ?>
                                 </p>
                             </div>
 
@@ -169,4 +211,20 @@
         </section><!-- .End product_detail -->
 
     </section><!--End #primary-->
-    <?php $this->load->view('layout/sidebar'); ?>
+<?php $this->load->view('layout/sidebar'); ?>
+    <script src="<?php echo base_url(); ?>public/homejs/idangerous.swiper-2.0.min.js"></script>
+    <script src="<?php echo base_url(); ?>public/homejs/idangerous.swiper.3dflow-2.0.js"></script>
+    <script>
+        var mySwiper = new Swiper('.swiper-container', {
+            slidesPerView: 3,
+            loop: true,
+            //Enable 3D Flow
+            tdFlow: {
+                rotate: 30,
+                stretch: 10,
+                depth: 150,
+                modifier: 1,
+                shadows: true
+            }
+        })
+    </script>
