8e1765af3592002f8328ef722c3848e871cca5f7
thanhdo.trinh@gmail.com <thanhdo.trinh@gmail.com@1fa18133-fb69-3655-42be-f4629fbfda3d>
2014-03-19 11:25:43 +0000

    sua home/views/home.php

--- application/modules/home/views/home.php
+++ application/modules/home/views/home.php
@@ -3,7 +3,7 @@
         <div class="swiper-wrapper">
             <?php foreach ($data_slide as $slide) { $images = json_decode($slide->images); ?>
             <div class="swiper-slide" style="background-image:url(<?php echo base_url().$images[0]; ?>)">
-                <a href="#" target="_blank"></a><!--
+                <a href="<?php echo site_url("home/cproducts/showDetailProducts/$slide->id/$slide->categoriesID"); ?>"></a><!--
             --></div>
             <?php }?>
         </div>
@@ -27,11 +27,11 @@
             
             <section class="module">
                 <div class="module_item clearfix">
-                    <a href="#" class="img_module">
+                    <a href="<?php echo site_url("home/cproducts/showDetailProducts/$value->id/$value->categoriesID"); ?>" class="img_module">
                         <img src="<?php echo base_url().$img[0]; ?>" alt="<?php echo $value->name ?>"/>
                     </a>
                     <div class="reduced">
-                        <header class="title_item"><a href="#"><?php echo $value->name ?></a></header>
+                        <header class="title_item"><a href="<?php echo site_url("home/cproducts/showDetailProducts/$value->id/$value->categoriesID"); ?>"><?php echo $value->name ?></a></header>
                         <p><?php echo substr($value->intro2,0,strrpos($value->intro2, ' ')); ?>...</p>
                     </div><!--End .reduced-->
                     <a href="<?php echo site_url("home/cproducts/showDetailProducts/$value->id/$value->categoriesID"); ?>" class="btn_readmore">Chi tiết</a>
@@ -46,8 +46,8 @@
         <button class="btn_showmore">Xem thêm</button>
     </div>
 
-    <script src="<?php echo base_url(); ?>template/js/idangerous.swiper-2.0.min.js"></script>
-    <script src="<?php echo base_url(); ?>template/js/idangerous.swiper.3dflow-2.0.js"></script>
+    <script src="<?php echo base_url(); ?>public/homejs/idangerous.swiper-2.0.min.js"></script>
+    <script src="<?php echo base_url(); ?>public/homejs/idangerous.swiper.3dflow-2.0.js"></script>
     <script>
         var mySwiper = new Swiper('.swiper-container', {
             slidesPerView: 3,
